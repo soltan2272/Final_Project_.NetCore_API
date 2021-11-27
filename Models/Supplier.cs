473ccc8da0fb8d38d83a262f5557bdea14994bb7
Elamir-Nady <ElamirNady@gmail.com>
2021-11-27 18:59:57 +0200

    create class (Supplier,Product,Store)

---
+++ Models/Supplier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Supplier
+    {
+        public int SSN { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Credit_Card { get; set; }
+    }
+}
