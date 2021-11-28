6668049f02bbd545e37667808edad48dda06ec9d
Bishoy-Shnouda <Bishoypixel@gmail.com>
2021-11-28 21:38:44 +0200

    public my classes

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    class Contact
+  public class Contact
     {
         public int ID { get; set; }
         public string Address { get; set; }
