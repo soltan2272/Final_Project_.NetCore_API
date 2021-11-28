ca66cd7531716af0327d387f37b173f7126dcb88
Soltan Salah <ssas2584@gmail.com>
2021-11-29 00:26:02 +0200

    ADD_Relationship_Soltan_Ali

--- Models/Offer.cs
+++ Models/Offer.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    class Offer
+   public class Offer
     {
         public int ID { get; set; }
         public DateTime Start_Date {get; set;}
         public DateTime End_Date { get; set; }
         public float Discount_Percentage { get; set; }
-        public float Current_Price { get; set; }
+
+        public IList<ProductOffer> ProductOffers { get; set; }
 
     }
 }
